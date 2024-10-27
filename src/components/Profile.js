9cde1de27ed77b890cf98cd9bf6218ba0da74af6
Anthony <artatum2024@gmail.com>
2024-10-27 16:34:53 -0400

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -39,66 +39,66 @@ export default function Profile () {
 		},
 	];
 
-    const [data, updateData] = useState(sampleData); //useState([]);
+    	const [data, updateData] = useState(sampleData); //useState([]);
 	const [dataFetched, updateFetched] = useState(false);
-    const [address, updateAddress] = useState("0x");
-    const [totalPrice, updateTotalPrice] = useState("0");
+    	const [address, updateAddress] = useState("0x");
+    	const [totalPrice, updateTotalPrice] = useState("0");
 
-    async function getNFTData(tokenId) {
-        const ethers = require("ethers");
-        let sumPrice = 0;
+    	async function getNFTData(tokenId) {
+        	const ethers = require("ethers");
+        	let sumPrice = 0;
 
-        //After adding your Hardhat network to your metamask, this code will get providers and signers
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        const signer = provider.getSigner();
-        const addr = await signer.getAddress();
-
-        //Pull the deployed contract instance
-        let contract = new ethers.Contract(AuthJSON.address, AuthJSON.abi, signer)
-
-        //create an NFT Token
-        let transaction = await contract.getMyNFTs()
-
-        /*
-        * Below function takes the metadata from tokenURI and the data returned by getMyNFTs() contract function
-        * and creates an object of information that is to be displayed
-        */
+	        //After adding your Hardhat network to your metamask, this code will get providers and signers
+	        const provider = new ethers.providers.Web3Provider(window.ethereum);
+	        const signer = provider.getSigner();
+	        const addr = await signer.getAddress();
+	
+	        //Pull the deployed contract instance
+	        let contract = new ethers.Contract(AuthJSON.address, AuthJSON.abi, signer)
+	
+	        //create an NFT Token
+	        let transaction = await contract.getMyNFTs()
+	
+	        /*
+	        * Below function takes the metadata from tokenURI and the data returned by getMyNFTs() contract function
+	        * and creates an object of information that is to be displayed
+	        */
         
-        const items = await Promise.all(transaction.map(async i => {
-            const tokenURI = await contract.tokenURI(i.tokenId);
-            let meta = await axios.get(tokenURI);
-            meta = meta.data;
+	        const items = await Promise.all(transaction.map(async i => {
+	            const tokenURI = await contract.tokenURI(i.tokenId);
+	            let meta = await axios.get(tokenURI);
+	            meta = meta.data;
+	
+	            let price = ethers.utils.formatUnits(i.price.toString(), 'ether');
+	            let item = {
+	                price,
+	                tokenId: i.tokenId.toNumber(),
+	                seller: i.seller,
+	                owner: i.owner,
+	                file: meta.image,
+	                name: meta.name,
+	                description: meta.description,
+	            }
+	            sumPrice += Number(price);
+	            return item;
+	        }))
+	
+	        updateData(items);
+	        updateFetched(true);
+	        updateAddress(addr);
+	        updateTotalPrice(sumPrice.toPrecision(3));
+    	};
 
-            let price = ethers.utils.formatUnits(i.price.toString(), 'ether');
-            let item = {
-                price,
-                tokenId: i.tokenId.toNumber(),
-                seller: i.seller,
-                owner: i.owner,
-                file: meta.image,
-                name: meta.name,
-                description: meta.description,
-            }
-            sumPrice += Number(price);
-            return item;
-        }))
-
-        updateData(items);
-        updateFetched(true);
-        updateAddress(addr);
-        updateTotalPrice(sumPrice.toPrecision(3));
-    }
-
-    const params = useParams();
-    const tokenId = params.tokenId;
+    	const params = useParams();
+    	const tokenId = params.tokenId;
 	
 	function List({ children }) {
-	  return (
+	  	return (
 		<ul className="divide-y divide-slate-100">
 		  {children}
 		</ul>
-	  )
-	}
+	  	)
+	};
 	
 	if(!dataFetched)
 		try {
@@ -107,41 +107,39 @@ export default function Profile () {
 		catch(e) {
 			alert( "Error Retrieving Hash" )
 			updateData([]);
-		}
+		};
     
     return (
-        <div class="profileClass justify-center bg-gradient-to-t from-white" style={{"max-height":"125vh"}}>
-            <div class="flex flex-col bg-stone-800/50 ml-5 mb-10 mt-10 mr-5 md:mt-20 md:mb-20 md:ml-24 md:mr-24 items-center justify-center rounded-lg shadow-2xl">
-            <div class="flex text-center flex-col mt-5 md:text-2xl text-black">
-			<div class="flex flex-col text-center items-center mt-4 mb-3 border-b border-slate-200">
-                <div class="flex flex-col max-w-md mb-5 ">
-                    <h2 class="text-md">Wallet Address</h2>
-					<p class="flow-text break-all text-sm">{address}</p>
-                </div>
-			</div>
-            </div>
-            <div class="flex flex-row text-center justify-center mt-10 md:text-2xl text-black">
-                    <div>
-                        <h2 class="font-bold">{address !== "0x" ? "Files":"Demo Files"} </h2>
-                        {data.length}
-                    </div>
-
-            </div>
-            <div class="flex flex-col text-center items-center mt-10 md:text-2xl text-black">
-          
-			<div class="divide-y-2 divide-black ml-24 mt-5 mb-12">
-			  <List>
-				{data.map((value, index) => (
-				  <ListItem data={value} key={index}/>
-				))}
-			  </List>
-			</div>
+	<div class="profileClass justify-center bg-gradient-to-t from-white" style={{"max-height":"125vh"}}>
+        <div class="flex flex-col bg-stone-800/50 ml-5 mb-10 mt-10 mr-5 md:mt-20 md:mb-20 md:ml-24 md:mr-24 items-center justify-center rounded-lg shadow-2xl">
+        <div class="flex text-center flex-col mt-5 md:text-2xl text-black">
+	<div class="flex flex-col text-center items-center mt-4 mb-3 border-b border-slate-200">
+        <div class="flex flex-col max-w-md mb-5 ">
+        	<h2 class="text-md">Wallet Address</h2>
+		<p class="flow-text break-all text-sm">{address}</p>
+        </div>
+	</div>
+        </div>
+        <div class="flex flex-row text-center justify-center mt-10 md:text-2xl text-black">
+        <div>
+                <h2 class="font-bold">{address !== "0x" ? "Files":"Demo Files"} </h2>
+                {data.length}
+        </div>
+        </div>
+        <div class="flex flex-col text-center items-center mt-10 md:text-2xl text-black">
+		<div class="divide-y-2 divide-black ml-24 mt-5 mb-12">
+		<List>
+			{data.map((value, index) => (
+			<ListItem data={value} key={index}/>
+			))}
+		</List>
+		</div>
                 <div class="mt-10 text-xl">
-                    {data.length === 0 ? "No data to display. You might not be logged in or you haven't moved any files to your wallet.":""}
+                	{data.length === 0 ? "No data to display. You might not be logged in or you haven't moved any files to your wallet.":""}
                 </div>
-            </div>
-            </div>
-		<Footer/>
+	</div>
+        </div>
+	<Footer/>
         </div>
     )
 };
