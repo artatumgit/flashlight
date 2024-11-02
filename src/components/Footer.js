727a62c4a712a120438918fde8db55eace5b4120
Anthony <artatum2024@gmail.com>
2024-11-02 05:09:38 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,44 +1,43 @@
 import React from "react";
 import {
-  Box,
-  Container,
-  Row,
-  Column,
-  FooterLink,
-  Heading,
+	Box,
+	Container,
+	Row,
+	Column,
+	FooterLink,
+	Heading,
 } from "./FooterStyles";
-  
-const Footer = () => {
-  return (
-    <Box>
 
-      <Container>
-        <Row>
-          <Column>
-            <Heading>About</Heading>
-            <FooterLink href="">Footer stuff</FooterLink>
+const Footer = () => {
+return (
+<Box>
+<Container>
+<Row>
+	<Column>
+		<Heading>About</Heading>
+		<FooterLink href="">Footer stuff</FooterLink>
 			<FooterLink href="">Immutable library</FooterLink>
 			<FooterLink href="">Developers</FooterLink>
-          </Column>
-          <Column>
-            <Heading>Contact</Heading>
-          </Column>
-          <Column>
-            <Heading>Build</Heading>
-          </Column>
-          <Column>
-            <Heading>Social Media</Heading>
-            <FooterLink href="">
-              <i className="fab fa-twitter">
-                <span style={{ marginLeft: "10px" }}>
-                  X
-                </span>
-              </i>
-            </FooterLink>
-          </Column>
-        </Row>
-      </Container>
-    </Box>
-  );
+	</Column>
+	<Column>
+		<Heading>Contact</Heading>
+	</Column>
+	<Column>
+		<Heading>Build</Heading>
+	</Column>
+	<Column>
+		<Heading>Social Media</Heading>
+		<FooterLink href="">
+			<i className="fab fa-twitter">
+			<span style={{ marginLeft: "10px" }}>
+			X
+			</span>
+			</i>
+		</FooterLink>
+	</Column>
+</Row>
+</Container>
+</Box>
+);
 };
 export default Footer;
